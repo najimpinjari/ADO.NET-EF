20696ff2cc6b7665e5f8496fbe47251bac37f1ae
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-02-29 07:13:29 +0530

    normal setup

---
+++ DOT.NET_28/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;  
+using System.Data.SqlClient;
+
+namespace DOT.NET_28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Connection();
+            Console.ReadLine();
+        }   
+        
+        static void Connection() // for making connection string 
+        {
+            string cs = "Data Source=DESKTOP-LQCK449\\SQLEXPRESS;Initial Catalog=ADODOTNET;Integrated Security=true;";
+
+            SqlConnection con = new SqlConnection(cs);
+            try
+            {
+                con.Open();
+
+                if (con.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("connection has been created successfully ");
+                }
+    
+            }catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
