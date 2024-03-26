20def9fc49ef44099eaf1df4bf7b58ef647181bf
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-03-26 17:14:11 +0530

    Add files via upload

---
+++ Solid_principles/Solid_princ_L/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_princ_L
+{
+    class Bird
+    {
+        public virtual void Fly()
+        {
+            Console.WriteLine("the Bird  is flayig.... ");
+
+            Console.ReadLine();
+        }
+    }
+    class Eagle : Bird
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("Eagle is flying high......");
+
+        }
+    }
+
+    class PEnguin : Bird
+    {
+        // penguin cannot fly so overrind fly method is not suitable 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bird eagle = new Eagle();
+            Bird penguin = new PEnguin();
+
+            eagle.Fly();
+            penguin.Fly();
+        }
+    }
+}
