1f3de19d29e85d2d4da47c1b3215171af3774688
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-03-26 17:15:50 +0530

    Add files via upload

---
+++ Solid_principles/Solic_Princ_I/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solic_Princ_I
+{
+
+    //interface for prenter
+    interface Iprenter
+    {
+        void Print();
+        void Scan();
+    }
+
+    //class implimenting printing   
+
+    class Printer : Iprenter
+    {
+        public void Print()
+        {
+            Console.WriteLine("printing");
+          //  Console.ReadLine();
+        }
+        public void Scan()
+        {
+            Console.WriteLine("Scanning");
+
+            Console.ReadLine();
+        }
+    }
+
+    class BesicPrinter : Iprenter
+    {
+        public void Print()
+        {
+            Console.WriteLine("printing");
+        }
+        public void Scan()
+        {
+          //  Console.WriteLine("");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Iprenter iprenter = new Printer();
+            iprenter.Print();
+            iprenter.Scan();
+
+            Iprenter besicPrinter = new BesicPrinter();
+            besicPrinter.Print();
+            besicPrinter.Scan();
+
+        }
+    }
+}
